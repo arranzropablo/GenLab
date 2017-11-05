88498de81346121bdec3eb17b36d26a1b2506beb
caminosajalberto <jalbercamino@gmail.com>
2017-11-05 20:18:39 +0100

    added login template and modified logincontroller

--- server-application/src/main/java/com/genlab/serverapplication/controllers/LoginController.java
+++ server-application/src/main/java/com/genlab/serverapplication/controllers/LoginController.java
@@ -1,8 +1,18 @@
 package com.genlab.serverapplication.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-
+	
+	@RequestMapping(value = "/login", method=RequestMethod.GET)
+	public String getLoginList(HttpServletRequest request, Model model) {
+		return "login";
+	}
 }
