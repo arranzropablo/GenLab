31d271395215e3e67fa22c424c66e437414a86ab
CharlieGnomo <carlomaroficial@gmail.com>
2017-11-26 12:05:23 +0100

    TheoryService and TheoryRepository added

---
+++ server-application/src/main/java/com/genlab/serverapplication/repositories/TheoryRepository.java
@@ -0,0 +1,8 @@
+package com.genlab.serverapplication.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.genlab.serverapplication.models.Theory;
+
+public interface TheoryRepository extends CrudRepository<Theory, Long>{
+
+}
