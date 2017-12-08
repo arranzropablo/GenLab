24d1365eaef5d4512770f558ea78ce74467a2a73
CharlieGnomo <carlomaroficial@gmail.com>
2017-12-08 12:41:19 +0100

    Test Repository and Service , and added <th>Editorial</th> in RecommendedBooks Table

--- server-application/src/main/java/com/genlab/serverapplication/models/TestQuestion.java
+++ server-application/src/main/java/com/genlab/serverapplication/models/TestQuestion.java
@@ -1,21 +1,52 @@
 package com.genlab.serverapplication.models;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
+@AllArgsConstructor
 @RequiredArgsConstructor
-public class TestQuestion {
+@NoArgsConstructor
+@Entity
+@Table(name="questions")
+public class TestQuestion implements Serializable{
 	
-	private long id;
+	private static final long serialVersionUID = 403919529582470981L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="texto")
 	@NonNull
 	private String statement;
+	
+	@OneToMany(targetEntity=TestAnswer.class, mappedBy="pregunta", cascade=CascadeType.ALL)
 	@NonNull
 	private List<TestAnswer> answers;	
+	
+	@ManyToOne(targetEntity=Test.class)
+	private Test test;
 	
 }
---
+++ server-application/src/main/java/com/genlab/serverapplication/services/TestQuestionService.java
@@ -0,0 +1,19 @@
+package com.genlab.serverapplication.services;
+
+import java.util.List;
+import com.genlab.serverapplication.models.TestQuestion;
+
+public interface TestQuestionService {
+	
+	public List<TestQuestion> getAllQuestions();
+	
+	public List<TestQuestion> getAllQuestionsByTest(int id);
+	
+	public TestQuestion getQuestion(int id);
+	
+	public int saveQuestion(TestQuestion t);
+	
+	public void deleteQuestion(int id);
+	
+	public boolean existsQuestion(int id);
+}
