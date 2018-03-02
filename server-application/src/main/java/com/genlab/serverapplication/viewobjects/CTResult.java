60c444990f09e0ae560822518c40eb25d1b0efaf
CharlieGnomo <carlomaroficial@gmail.com>
2018-03-02 17:26:55 +0100

    added Maps in CTResult.java

--- server-application/src/main/java/com/genlab/serverapplication/viewobjects/CTResult.java
+++ server-application/src/main/java/com/genlab/serverapplication/viewobjects/CTResult.java
@@ -9,7 +9,12 @@ import lombok.Data;
 @Builder
 public class CTResult {
 	
-	private Map<String , Double> expectedValues;
-	private Map<String, Double> values;
+	private Map<String,Double> expectedValues;
+	private Map<String,Double> values;
+	private Map<String,Double> observed;
+	private Map<String,String> agree;
+	private Map<String,String> phase;
+	private Map<String,String> valuesString;
+	private String result;
 	
 }
